ceb2ae4b85050264d1353c79a32aed437c02c3a2
Kees van Reeuwijk <keesvr@xs4all.nl>
2023-01-19 00:26:11 +0100

    No special queue.

--- primes-mpi-sbatch.sh
+++ primes-mpi-sbatch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 #SBATCH -t 3:00 -N 1 --mem=100M
-#SBATCH --reservation=uva_course
 
 mpirun ./primes-mpi
--- stencil-mpi-sbatch.sh
+++ stencil-mpi-sbatch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 #SBATCH -t 1:00 -N 1 --mem=200M
-#SBATCH --reservation=uva_course
 
 mpirun ./stencil-mpi
