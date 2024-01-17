e09c4966af0e4d8c7ae35574ff1b5edf797947fe
Kees van Reeuwijk <keesvr@xs4all.nl>
2024-01-17 23:11:56 +0100

    More fixes.

--- primes-mpi-sbatch.sh
+++ primes-mpi-sbatch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 #SBATCH --partition=thin_course
-#SBATCH -t 3:00 --ntasks=16 --mem=100M
+#SBATCH -t 3:00 --ntasks=16
 
 module purge
 module load 2023
 module load MPICH/4.1.2-GCC-12.3.0
 
-srun --mpi=pmi2 ./primes-mpi
+mpirun ./primes-mpi
--- stencil-mpi-sbatch.sh
+++ stencil-mpi-sbatch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 #SBATCH --partition=thin_course
-#SBATCH -t 1:00 --ntasks=16 --mem=200M
+#SBATCH -t 1:00 --ntasks=16
 
 module purge
 module load 2023
 module load MPICH/4.1.2-GCC-12.3.0
 
-srun --mpi=pmi2 ./stencil-mpi
+mpirun ./stencil-mpi
