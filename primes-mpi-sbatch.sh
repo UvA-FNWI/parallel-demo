343910fc93fb73c20fb2c91065a11dff529a60f8
Kees van Reeuwijk <keesvr@xs4all.nl>
2024-01-17 23:09:53 +0100

    Get mpi sbatch scripts working again.

--- primes-mpi-sbatch.sh
+++ primes-mpi-sbatch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 #SBATCH --partition=thin_course
-#SBATCH -t 3:00 --ntasks=16 --mem=10M
+#SBATCH -t 3:00 --ntasks=16 --mem=100M
+
+module purge
+module load 2023
+module load MPICH/4.1.2-GCC-12.3.0
 
 srun --mpi=pmi2 ./primes-mpi
--- stencil-mpi-sbatch.sh
+++ stencil-mpi-sbatch.sh
@@ -2,4 +2,8 @@
 #SBATCH --partition=thin_course
 #SBATCH -t 1:00 --ntasks=16 --mem=200M
 
+module purge
+module load 2023
+module load MPICH/4.1.2-GCC-12.3.0
+
 srun --mpi=pmi2 ./stencil-mpi
